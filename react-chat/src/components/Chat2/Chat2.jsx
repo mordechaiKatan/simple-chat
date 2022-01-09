45019d801fbef854586026596ecb1d3a001c81c6
hayehudy <67315310+hayehudy@users.noreply.github.com>
2022-01-10 00:33:55 +0200

    exp5

--- react-chat/src/components/Chat2/Chat2.jsx
+++ react-chat/src/components/Chat2/Chat2.jsx
@@ -2,7 +2,7 @@ import {useState, useEffect} from "react"
 import "./Chat2.css"
 import MessageItem from "../MessageItem/MessageItem";
 import axios from 'axios';
-import SendIcon from '@material-ui/icons/Send';
+// import SendIcon from '@material-ui/icons/Send';
 
 
 let Chat2 = ({ messages, addMessage})=>{
@@ -22,7 +22,7 @@ let Chat2 = ({ messages, addMessage})=>{
                {messages && messages.map((m, index)=><MessageItem key={index} text={m}/>)}
             </div>
             <div className="futer" >
-                <SendIcon style={{transform: "rotate(-180deg)"}} onClick={()=>{addMessage(value); setValue("")}}/>                
+                {/* <SendIcon style={{transform: "rotate(-180deg)"}} onClick={()=>{addMessage(value); setValue("")}}/>                 */}
                 <input className="chat-input" value={value} onChange={(e)=>setValue(e.target.value)}  onKeyDown={handleKeyDown}></input>        
                 {/* <button onClick={clear}>CLEAR CHAT</button> */}
             </div>
