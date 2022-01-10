127a1b3e1255d49f355652589a3e1ae7d9900699
hayehudy <67315310+hayehudy@users.noreply.github.com>
2022-01-10 14:57:49 +0200

    fix icons2

--- react-chat/src/components/Chat2/Chat2.jsx
+++ react-chat/src/components/Chat2/Chat2.jsx
@@ -2,7 +2,8 @@ import {useState, useEffect} from "react"
 import "./Chat2.css"
 import MessageItem from "../MessageItem/MessageItem";
 import axios from 'axios';
-import icon from "./Arrow_icon.png";
+// import icon from "./Arrow_icon.png";
+import icon from "./arrow2.png"
 import icon2 from "./5211.png_860.png";
 import xicon from "./xicon.png"
 
@@ -35,9 +36,8 @@ let Chat2 = ({ messages})=>{
             </div>
             <div className="futer" >
                 {value
-                ? <img style={{height: 40, width: 40, transform: "rotate(-180deg)"}} src={icon} onClick={send}/>
-                // : <img style={{height:40, width:40}} src={xicon}/>}
-                : <img style={{height:40, width:40}} src={icon}/>}
+                ? <img style={{height: 40, width: 40}} src={icon} onClick={send}/>
+                : <img style={{height:40, width:40}} src={xicon}/>}
                 <input className="chat-input" value={value} onChange={(e)=>setValue(e.target.value)}  onKeyDown={handleKeyDown}></input>        
                 <img src={icon2} style={{width:40,height:40}} onClick={clear}/>
             </div>
