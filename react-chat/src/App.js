877f13c5f2d67eaab9341c2f8167eeb22feeda66
hayehudy <67315310+hayehudy@users.noreply.github.com>
2022-01-10 14:07:22 +0200

    adding x-icon

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -6,16 +6,18 @@ import Chat from "./components/Chat/Chat";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import reducer from "./redux/reducer";
-import Chat2 from "./components/Chat2/Chat2"
+import Chat2 from "./components/Chat2/Chat2";
 
 
 function App() {
 
-  const [initialState, setInitialState]=useState({messages:["hello!", "how are you", "whats your name"]})
-  const [initialState2, setInitialState2]=useState()
+  // const [initialState, setInitialState]=useState({messages:["hello!", "how are you", "whats your name"]});
+  // const store = createStore (reducer, initialState);
+
+  const [messages, setMessages]=useState()
 
   useEffect(()=> 
-    axios.get("/api/get").then((res)=>{setInitialState2(res.data)})
+    axios.get("/api/get").then((res)=>{ setMessages(res.data)})
     ,[])
     
   useEffect(()=> {
@@ -24,24 +26,21 @@ function App() {
       console.log(`I'm connected with the back-end`);
     });
     socket.on('newMessages', (data)=>{
-      setInitialState2([...data], console.log(data))
+      setMessages([...data]); console.log(data)
     });
-    socket.on('clear', ()=> setInitialState2([]))
+    socket.on('clear', ()=>  setMessages([]))
   },[])
 
-  function addMessage (newMessage) {
-    axios.post("/api/post", {newMessage: newMessage} )
-  }
-
-  const store = createStore (reducer, initialState)
+ 
+ 
 
   return (
 
     <div className="App">    
-      {initialState2 && <Chat2 messages={initialState2} addMessage={addMessage}></Chat2>}  
+      {messages && <Chat2 messages={messages}></Chat2>}  
       {/* <Provider store={store}>
         <Chat></Chat>
-      </Provider> */}
+        </Provider> */}
     </div>
   );
 }
--- react-chat/src/components/Chat2/Chat2.jsx
+++ react-chat/src/components/Chat2/Chat2.jsx
@@ -3,18 +3,28 @@ import "./Chat2.css"
 import MessageItem from "../MessageItem/MessageItem";
 import axios from 'axios';
 import icon from "./Arrow_icon.png";
-import icon2 from "./5211.png_860.png"
+import icon2 from "./5211.png_860.png";
+import xicon from "./xicon.png"
 
 
-let Chat2 = ({ messages, addMessage})=>{
+let Chat2 = ({ messages})=>{
    
     const [value,setValue]=useState("");
 
-    let handleKeyDown = (e)=> {if (e.key === 'Enter') {addMessage(value); setValue("")}}
+    let handleKeyDown = (e)=> {if (e.key === 'Enter') {send()}}
+
+    function addMessage (newMessage) {
+        axios.post("/api/post", {newMessage: newMessage} )
+      }
 
     function clear () {
         axios.get("/api/clear")
         console.log("sababa")
+    }
+
+    function send () {
+        addMessage(value);
+        setValue("")
     }
 
     return (
@@ -24,10 +34,11 @@ let Chat2 = ({ messages, addMessage})=>{
                {messages && messages.map((m, index)=><MessageItem key={index} text={m}/>)}
             </div>
             <div className="futer" >
-                <img style={{height: 40, width: 40, transform: "rotate(-180deg)"}} src={icon} onClick={()=>{addMessage(value); setValue("")}}/>
+                {value
+                ? <img style={{height: 40, width: 40, transform: "rotate(-180deg)"}} src={icon} onClick={send}/>
+                : <img style={{height:40, width:40}} src={xicon}/>}
                 <input className="chat-input" value={value} onChange={(e)=>setValue(e.target.value)}  onKeyDown={handleKeyDown}></input>        
                 <img src={icon2} style={{width:40,height:40}} onClick={clear}/>
-                {/* <button >CLEAR CHAT</button> */}
             </div>
         </div>
     )
