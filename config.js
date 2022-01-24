b17689b85b5a41ad1d52a47f20e515395171cb78
hayehudy <67315310+hayehudy@users.noreply.github.com>
2022-01-24 09:56:17 +0200

    connect to mongo atlas

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ encoding: 'utf8' });
 
 module.exports = {
-    mongoUri: process.env.NODE_ENV === 'production' ? process.env.MONGO_URI : 'mongodb://localhost:27017/moty-chat',
+    mongoUri: process.env.MONGO_URL || 'mongodb://localhost:27017/moty-chat',
     port: process.env.PORT || 8080
   }
