91a8a483120bc18e908818822ba4dd77fb39f70a
hayehudy <67315310+hayehudy@users.noreply.github.com>
2022-01-26 18:28:51 +0200

    add validation to user schema

--- models/User.js
+++ models/User.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  userName: String,
-  userId: String
+  userName: {
+  type: String,
+  required: true,
+  maxLength:8,
+  minlength: 3,
+  // match: /^(?!\s*$).+/,
+  trim: true,
+
+},
+  userId:{
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100
+  }
 })
 
 const User = mongoose.model('User', UserSchema);
